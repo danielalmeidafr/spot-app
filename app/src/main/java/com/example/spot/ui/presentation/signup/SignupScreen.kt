0c99f48ea417db6e61609d6a51d3ac7710b00017
danielalmeidafr <d.almeidafrr@gmail.com>
2025-08-27 10:29:12 -0300

    feat: add login and signup page

--- app/src/main/java/com/example/spot/ui/presentation/signup/SignupScreen.kt
+++ app/src/main/java/com/example/spot/ui/presentation/signup/SignupScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -31,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import com.example.spot.ui.presentation.components.CustomTextField
