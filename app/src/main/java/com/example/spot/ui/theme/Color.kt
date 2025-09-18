0a148829e6a83818562bdfd76774e96fab32a015
danielalmeidafr <d.almeidafrr@gmail.com>
2025-09-17 22:47:57 -0300

    style: change the color on light mode to texts

--- app/src/main/java/com/example/spot/ui/theme/Color.kt
+++ app/src/main/java/com/example/spot/ui/theme/Color.kt
@@ -14,6 +14,6 @@ val White50 = Color(0x80FFFFFF)
 // LIGHT MODE
 val WhiteBack = Color(0xFFEAEAEA)
 val WhiteVariant = Color(0xFFF0F0F0)
-val Black = Color(0xFF595959)
+val Black = Color(0xFF2A2A2A)
 val Black04 = Color(0x112A2A2A)
 val Black50 = Color(0x802A2A2A)
